c574e8a6d1c291aeb7e923afeb0b6f5ddf234c0c
shash <shashanka5398@gmail.com>
2024-12-05 21:29:10 +0530

    updated mapped title match in database titles

--- database/__init__.py
+++ database/__init__.py
@@ -28,14 +28,15 @@ class Database:
         starts_with = []
         contains = []
         # Correct MongoDB query with $regex
+        title = title.lower()
         async for document in self.collection.find({"titles": {"$regex": re.escape(title), "$options": "i"}}):
-            titles = document.get('titles', [])
+            titles = [i.lower() for i in document.get('titles', [])]
             if any(t.startswith(title) for t in titles):
                 starts_with.insert(0, document)
             elif any(title in t for t in titles):
                 contains.append(document)
         async for document in self.collection.find({"title": {"$regex": re.escape(title), "$options": "i"}}):
-            titles = document.get('titles', [])
+            titles = [i.lower() for i in document.get('titles', [])]
             if any(t.startswith(title) for t in titles):
                 starts_with.insert(0, document)
             elif any(title in t for t in titles):
