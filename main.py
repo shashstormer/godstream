41c6d0c87230df576c55f15b77844365e20bb859
shash <shashanka5398@gmail.com>
2024-12-11 12:26:32 +0530

    fixed error when no mongo url in env

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ streamable_cache = Cacher("streamable_anime")
 sources_cache = Cacher()
 # streamable_cache = Cacher()
 recent_cache = Cacher("recent_pages")
+
+
 # cors_cache = Cacher("cors_cache")
-cors_cache = Cacher()
-mapper_utils = MapperUtils()
 
 
 @app.get("/")
@@ -54,7 +54,6 @@ async def exit_pgm():
     return {"status": "unknown"}
 
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # Adjust this to your frontend URL
@@ -65,26 +64,28 @@ app.add_middleware(
 
 add_streamable(app, cache_store=streamable_cache)
 if Config.enable_cors_proxy:
+    cors_cache = Cacher()
     executor, request_executor, cors_files_cache = add_cors(app, "*", cors_cache)
 if Config.mongo_url:
+    mapper_utils = MapperUtils()
     mapper_obj = add_mappers(app, streamable_cache=streamable_cache)
     mapped_obj = add_mapped(app, streamable_cache=streamable_cache, mapper_utils=mapper_utils,
                             sources_cache=sources_cache)
     # threading.Thread(target=mapper_utils.create_map_multithread, args=(streamable_cache, recent_cache, 10)).start()
-add_user_datastorage(app)
+    add_user_datastorage(app)
 
 print("Running on http://127.0.0.1:5636")
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5636)
     streamable_cache.stop_cleanup_thread()
     recent_cache.stop_cleanup_thread()
-    cors_cache.stop_cleanup_thread()
-    mapper_utils.running = False
     if Config.enable_cors_proxy:
+        cors_cache.stop_cleanup_thread()
         cors_files_cache.stop_cleanup_thread()
         executor.shutdown(wait=False, cancel_futures=True)
         request_executor.shutdown(wait=False, cancel_futures=True)
     if Config.mongo_url:
+        mapper_utils.running = False
         mapper_obj.database.shutdown()
         mapped_obj.database.shutdown()
     while True:
